4fed28dd7ee9cac765924ba9a41ac0fda4ca29a1
Pedro Vitor Lima Novaes <pedro.novaes@bandtec.com.br>
2020-12-11 08:06:41 -0300

    Simple requests created, and tested with the front end (Post-Del - Put - Get)

---
+++ WebApi/WebApi/Controllers/AlunoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [EnableCors("*", "*", "*")] // habilita os acessos: Origin - (Site); Header ; Methods - (limita o acesso das requisições para a url definida) 
+    public class AlunoController : ApiController
+    {
+        // GET: api/Aluno
+        public IEnumerable<Aluno> Get()
+        {
+            Aluno aluno = new Aluno();
+            return aluno.ListarAlunos();
+        }
+
+        // GET: api/Aluno/5
+        public Aluno Get(int id)
+        {
+            Aluno aluno = new Aluno();
+            return aluno.ListarAlunos().FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST: api/Aluno
+        public List<Aluno> Post([FromBody] Aluno aluno)
+        {
+            Aluno _aluno = new Aluno();
+
+            _aluno.Inserir(aluno);
+
+            return _aluno.ListarAlunos();
+        }
+
+        // PUT: api/Aluno/5
+        public Aluno Put(int id, [FromBody] Aluno aluno)
+        {
+            Aluno _aluno = new Aluno();
+             _aluno = _aluno.Atualizar(id, aluno);
+             
+            return _aluno;
+        }
+
+        // DELETE: api/Aluno/5
+        public void Delete(int id)
+        {
+            Aluno _aluno = new Aluno();
+            _aluno.Deletar(id);
+
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/Aluno.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace WebApi.Models
+{
+    public class Aluno
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Telefone { get; set; }
+        public int Ra { get; set; }
+
+
+        public List<Aluno> ListarAlunos ()
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data/Base.json");
+            var json = File.ReadAllText(caminhoArquivo);
+            List<Aluno> listaDeAlunos = JsonConvert.DeserializeObject<List<Aluno>>(json);
+
+            return listaDeAlunos;
+        }
+
+        public  bool ReescreverArquivo(List<Aluno> listAlunos) 
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data/Base.json");
+
+            var json = JsonConvert.SerializeObject(listAlunos, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+
+            return true;
+        }
+
+
+        public  Aluno Inserir(Aluno aluno)
+        {
+            var listaDeAlunos = ListarAlunos();
+
+            var maxId = listaDeAlunos.Max(al => al.Id);
+            aluno.Id = maxId + 1;
+            listaDeAlunos.Add(aluno);
+
+            ReescreverArquivo(listaDeAlunos);
+            return aluno;
+        }
+
+        public Aluno Atualizar (int id, Aluno aluno) 
+        {
+            var listaDeAlunos = ListarAlunos();
+
+            var itemIndex = listaDeAlunos.FindIndex(al => al.Id == aluno.Id);
+
+            if (itemIndex >= 0) 
+            {
+                aluno.Id = id;
+                listaDeAlunos[itemIndex] = aluno;
+            }
+            else{
+                return null;
+            }
+
+            ReescreverArquivo(listaDeAlunos);
+            return aluno;
+        }
+
+        public bool Deletar(int id)
+        {
+            var listaDeAlunos = ListarAlunos();
+
+            var itemIndex = listaDeAlunos.FindIndex(al => al.Id == id);
+
+            if (itemIndex >= 0)
+            {
+                listaDeAlunos.RemoveAt(itemIndex);
+            }
+            else{
+                return false;
+            }
+
+            ReescreverArquivo(listaDeAlunos);
+            return true;
+             
+        }
+    }
+}
